77385fcb059dfed37bec1a73f13f316192232b15
WasabyDev <wasabygay@gmail.com>
2024-05-29 02:58:26 +0900

    aaaa

--- www/script.js
+++ www/script.js
@@ -1,3 +1,8 @@
 document.querySelector(".hamburger").addEventListener("click", () =>
     document.querySelector(".container").classList.toggle("show-menu")
 );
+
+function redirecionarParaOutraPagina() {
+    // Substitua "outra-pagina.html" pelo URL da página para a qual deseja redirecionar
+    window.location.href = "alertas.html";
+}
